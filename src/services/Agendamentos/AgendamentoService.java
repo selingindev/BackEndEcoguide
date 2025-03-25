6d661d809277fba34d70d04f2a75fe4dbe27d494
Arthur Selingin <arthur_santos@outlook.com.br>
2025-03-24 22:24:57 -0300

    implementando agendamento service

--- src/services/Agendamentos/AgendamentoService.java
+++ src/services/Agendamentos/AgendamentoService.java
@@ -1,14 +1,52 @@
 package services.Agendamentos;
 
+import java.util.List;
+
+import Mapper.Agendamento.MapperAgendamento;
+import model.daos.agendamento.AgendamentoDAO;
 import model.dtos.AgendamentoDTO;
 import model.entidades.Agendamento;
+import services.IContratoService;
 
 /// meu service tem a funlça de converter antes do dto e o dao se comunicarem
-public class AgendamentoService {
+public class AgendamentoService implements IContratoService<AgendamentoDTO> {
     AgendamentoDTO agendamentoDTO;
+    AgendamentoDAO agendamentoDAO;
+    MapperAgendamento mapperAgendamento;
+    
+    @Override
+    public void criar(AgendamentoDTO aDto){
+        Agendamento agendamento = mapperAgendamento.toEntity(aDto);
+        agendamentoDAO.criar(agendamento);
+    }
 
-    public Agendamento criar(AgendamentoDTO aDto){
-        Agendamento agendamento =  agendamentoDTO.toAgendamento(aDto);     
-        return 
+    @Override
+    public Boolean excluir(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'excluir'");
+    }
+
+    @Override
+    public AgendamentoDTO alterar(AgendamentoDTO obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'alterar'");
+    }
+
+    @Override
+    public List listar(AgendamentoDTO obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+    }
+
+    @Override
+    public List listarFiltro(AgendamentoDTO obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarFiltro'");
+    }
+
+    @Override
+    public Object selecionarID(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'selecionarID'");
     }
 }
